fe41338977d4b5352d82a699f18bdda2d82540d5
Aggybf <160708864+Aggybf@users.noreply.github.com>
2024-02-21 19:16:39 +0530

    Add files via upload

---
+++ Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./top.module.css"
+const Top = () => {
+  return (
+    <div className={styles.top}>
+        <h3>If you want to develop, this type of website. Then call me +91 8298262107 #paid</h3>
+    </div>
+  )
+}
+
+export default Top
